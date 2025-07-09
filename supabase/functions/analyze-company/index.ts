7e12593b9adc2437a509e19279c9add3d4d8ace6
Patrick <patrick.soe@hotmail.com>
2025-07-10 02:01:38 +0700

    update analyze company function

--- supabase/functions/analyze-company/index.ts
+++ supabase/functions/analyze-company/index.ts
@@ -7,6 +7,24 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import OpenAI from "https://esm.sh/openai@4.20.1";
 
+const getUserId = async (req: Request, supabase: SupabaseClient) => {
+  const authHeader = req.headers.get("Authorization");
+  if (!authHeader) {
+    throw new Error("Authorization header is required");
+  }
+  const token = authHeader.replace("Bearer ", "");
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser(token);
+
+  if (authError || !user) {
+    throw new Error("Invalid or expired token");
+  }
+
+  return user.id;
+};
+
 function cleanHtmlContent(html: string): string | null {
   try {
     const textContent = html
@@ -38,44 +56,7 @@ Deno.serve(async (req) => {
       "https://lkkwcjhlkxqttcqrcfpm.supabase.co",
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxra3djamhsa3hxdHRjcXJjZnBtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTMxMzE5OCwiZXhwIjoyMDYwODg5MTk4fQ.e8SijEhKnoa1R8dYzPBeKcgsEjKtXb9_Gd1uYg6AhuA"
     );
-    // Authentication logic
-    let userId = null;
-    const authHeader = req.headers.get("Authorization");
-    if (!authHeader) {
-      return new Response(
-        JSON.stringify({ error: "Authorization header is required" }),
-        {
-          headers: { "Content-Type": "application/json" },
-          status: 401,
-        }
-      );
-    }
-
-    try {
-      const token = authHeader.replace("Bearer ", "");
-      const {
-        data: { user },
-        error: authError,
-      } = await supabase.auth.getUser(token);
-
-      if (authError || !user) {
-        return new Response(
-          JSON.stringify({ error: "Invalid or expired token" }),
-          {
-            headers: { "Content-Type": "application/json" },
-            status: 401,
-          }
-        );
-      }
-
-      userId = user.id;
-    } catch (error) {
-      return new Response(JSON.stringify({ error: "Authentication failed" }), {
-        headers: { "Content-Type": "application/json" },
-        status: 401,
-      });
-    }
-
+    const userId = await getUserId(req, supabase);
     const { campaign_id, company_name, company_website } = await req.json();
     if (!company_name || !company_website) {
       return new Response(
@@ -97,7 +78,7 @@ Deno.serve(async (req) => {
 
     await supabase
       .from("campaign_progress")
-      .update({ latest_step: 1 })
+      .update({ latest_step: 2 })
       .eq("id", campaignData.progress_id);
 
     const params = new URLSearchParams({
